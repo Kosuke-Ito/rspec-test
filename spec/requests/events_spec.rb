9a60231755553df06192a4be33c04f3483c86e52
Kosuke Ito <keree.it.owl.uk@gmail.com>
2022-02-15 16:07:02 +0900

    Factory createのタイミング変更

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -2,18 +2,6 @@ require "rails_helper"
 
 RSpec.describe "EventsController", :type => :request do
   let(:user) { create(:user) }
-  let(:event) { build(:event, owner_id: user.id )}
-  let(:params) {
-    {
-      event: {
-        name: event.name, 
-        place: event.place,
-        content: event.content, 
-        start_at: event.start_at, 
-        end_at: event.end_at
-      }
-    }
-  }
 
   before do
     Rails.application.env_config['omniauth.auth'] = set_omniauth(user)
@@ -21,22 +9,34 @@ RSpec.describe "EventsController", :type => :request do
   end
 
   describe "#create" do
+    let(:event) { build(:event) }
+    let(:params) {
+      {
+        event: {
+          name: event.name,
+          place: event.place,
+          content: event.content,
+          start_at: event.start_at,
+          end_at: event.end_at
+        }
+      }
+    }
     it "ログイン中のユーザーのEventが作られること" do
       expect{ post "/events/", params: params }.to change{ user.created_events.count }.from(0).to(1)
     end
   end
 
   describe "#update" do
+    let(:event) { create(:event, owner_id: user.id) }
     it "ログイン中のユーザーのEventがアップデートされること" do
-      event.save
       expect{ patch "/events/#{event.id}", params: { event: { name: "hogehoge" } } }.to change{ event.reload.name }.from("イベント名2").to("hogehoge")
     end
   end
 
   describe "#delete" do
-    it "ログイン中のユーザーのEventが削除されること" do      
-      event.save
-      expect{ delete "/events/#{event.id}" }.to change{ Event.all.size }.by(-1)
+    let!(:event) { create(:event, owner_id: user.id) }
+    it "ログイン中のユーザーのEventが削除されること" do
+      expect{ delete "/events/#{event.id}" }.to change{ user.created_events.count }.by(-1)
     end
   end
 
