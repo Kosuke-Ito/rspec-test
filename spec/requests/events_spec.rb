820fe46175daf7d84524ed83eef52afce42f794d
Kosuke Ito <keree.it.owl.uk@gmail.com>
2022-02-15 11:49:19 +0900

    構造の修正

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -1,50 +1,43 @@
 require "rails_helper"
 
 RSpec.describe "EventsController", :type => :request do
-  fdescribe "create" do
-    let(:user) { create(:user) }
-    let(:event) { build(:event, owner_id: user.id )}
-    let(:params) {
-      {
-        event: {
-          name: event.name, 
-          place: event.place,
-          content: event.content, 
-          start_at: event.start_at, 
-          end_at: event.end_at
-        }
+  let(:user) { create(:user) }
+  let(:event) { build(:event, owner_id: user.id )}
+  let(:params) {
+    {
+      event: {
+        name: event.name, 
+        place: event.place,
+        content: event.content, 
+        start_at: event.start_at, 
+        end_at: event.end_at
       }
     }
+  }
 
-    context "ログインしているとき" do
-      # パターン１
-      before do
-        Rails.application.env_config['omniauth.auth'] = set_omniauth(user)
-        get "/auth/:provider/callback"
-      end
+  before do
+    Rails.application.env_config['omniauth.auth'] = set_omniauth(user)
+    get "/auth/:provider/callback"
+  end
 
-      # パターン2
-      # before do
-      #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      # end
-
-      it "ログイン中のユーザーのEventが作られること" do
-        expect{ post "/events/", params: params }.to change{ user.created_events.count }.from(0).to(1)
-      end
-
-      it "ログイン中のユーザーのEventがアップデートされること" do
-        event.save
-        patch "/events/#{event.id}", params: { event: { name: "hogehoge" } }
-        expect(event.reload.name).to eq "hogehoge"
-      end
-
-      it "ログイン中のユーザーのEventが削除されること" do      
-        event.save
-        expect(Event.all.count).to eq 1
-        delete "/events/#{event.id}"
-        expect(Event.all.count).to eq 0
-      end
-      
+  describe "#create" do
+    it "ログイン中のユーザーのEventが作られること" do
+      expect{ post "/events/", params: params }.to change{ user.created_events.count }.from(0).to(1)
     end
   end
+
+  describe "#update" do
+    it "ログイン中のユーザーのEventがアップデートされること" do
+      event.save
+      expect{ patch "/events/#{event.id}", params: { event: { name: "hogehoge" } } }.to change{ event.reload.name }.from("イベント名2").to("hogehoge")
+    end
+  end
+
+  describe "#delete" do
+    it "ログイン中のユーザーのEventが削除されること" do      
+      event.save
+      expect{ delete "/events/#{event.id}" }.to change{ Event.all.size }.by(-1)
+    end
+  end
+
 end
